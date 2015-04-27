8c5dc12128e22901907359fd16c1e8e1db6e01ba
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-04-27 13:55:40 -0400

    Added general constructor settings tests

--- packages/itemsholdr/Tests/tests.js
+++ packages/itemsholdr/Tests/tests.js
@@ -1,5 +1,35 @@
-describe("StatsHoldr", function () {
+var prefix = "MyStatsHoldr",
+    autoSave = true,
+    callbackArgs = ["hello", "world"],
+    defaults = {
+        "valueDefault": 0,
+        "onModular": 10
+    },
+    displayChanges = {
+        "9": "max"
+    },
+    values = ["lives", "tries"],
+    StatsHolder;
+
+describe("constructor", function () {
     it("doesn't throw an error", function () {
         new StatsHoldr();
-    })
+    });
+
+    it("correctly sets general settings", function () {
+        StatsHolder = new StatsHoldr({
+            "prefix": prefix,
+            "autoSave": autoSave,
+            "callbackArgs": callbackArgs,
+            "defaults": defaults,
+            "displayChanges": displayChanges,
+            "values": values
+        });
+
+        chai.expect(StatsHolder.getPrefix()).to.be.equal(prefix);
+        chai.expect(StatsHolder.getAutoSave()).to.be.equal(autoSave);
+        chai.expect(StatsHolder.getCallbackArgs()).to.be.equal(callbackArgs);
+        chai.expect(StatsHolder.getDefaults()).to.be.equal(defaults);
+        chai.expect(StatsHolder.getDisplayChanges()).to.be.equal(displayChanges);
+    });
 })
