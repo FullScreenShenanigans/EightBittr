5f9c54624e6a4a20f18cb4cd27c0adda55040269
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-06-17 05:33:57 -0400

    Initial TypeScript scaffolding

---
+++ packages/sceneplayr/Gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        "pkg": grunt.file.readJSON("package.json"),
+        "meta": {
+            "paths": {
+                "source": "Source",
+                "dist": "Distribution"
+            }
+        },
+        "tslint": {
+            "options": {
+                "configuration": grunt.file.readJSON("tslint.json")
+            },
+            "files": {
+                "src": ["<%= meta.paths.source %>/<%= pkg.name %>.ts"]
+            }
+        },
+        "typescript": {
+            "base": {
+                "src": "<%= meta.paths.source %>/<%= pkg.name %>.ts",
+                "dest": "<%= meta.paths.source %>/<%= pkg.name %>.js"
+            }
+        },
+        "clean": ["<%= meta.paths.dist %>"],
+        "copy": {
+            "default": {
+                "files": [{
+                    "src": "<%= meta.paths.source %>/<%= pkg.name %>.js",
+                    "dest": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.js"
+                }, {
+                    "src": "<%= meta.paths.source %>/<%= pkg.name %>.ts",
+                    "dest": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.ts"
+                }, {
+                    "src": "<%= meta.paths.source %>/References/*.ts",
+                    "dest": "<%= meta.paths.dist %>/",
+                    "expand": true,
+                    "flatten": true
+                }, {
+                    "src": "README.md",
+                    "dest": "<%= meta.paths.dist %>/"
+                }, {
+                    "src": "README.md",
+                    "dest": "<%= meta.paths.source %>/"
+                }, {
+                    "src": "LICENSE.txt",
+                    "dest": "<%= meta.paths.dist %>/"
+                }, {
+                    "src": "LICENSE.txt",
+                    "dest": "<%= meta.paths.source %>/"
+                }]
+            }
+        },
+        "uglify": {
+            "options": {
+                "compress": true,
+                "sourceMap": true
+            },
+            "dist": {
+                "files": {
+                    "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.min.js": ["<%= meta.paths.source %>/<%= pkg.name %>.js"],
+                }
+            }
+        },
+        "preprocess": {
+            "dist": {
+                "src": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.ts",
+                "dest": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.ts"
+            }
+        },
+        "mocha_phantomjs": {
+            "all": ["Tests/*.html"]
+        }
+    });
+    
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-mocha-phantomjs");
+    grunt.loadNpmTasks("grunt-preprocess");
+    grunt.loadNpmTasks("grunt-tslint");
+    grunt.loadNpmTasks("grunt-typescript");
+    grunt.registerTask("default", [
+        "tslint", "typescript", "clean", "copy", "uglify", "preprocess", "mocha_phantomjs"
+    ]);
+};
---
+++ packages/sceneplayr/README.md
@@ -0,0 +1,40 @@
+# ScenePlayr [![Build Status](https://travis-ci.org/FullScreenShenanigans/ScenePlayr.svg?branch=master)](https://travis-ci.org/FullScreenShenanigans/ScenePlayr)
+
+A cutscene runner for jumping between scenes and their routines.
+
+
+## Build Process
+
+ScenePlayr uses [Grunt](http://gruntjs.com/) to automate building, which 
+requires [Node.js](http://node.js.org). The process is straightforward; see 
+[Grunt's help page](http://gruntjs.com/getting-started) for more info.
+
+To build from scratch, install NodeJS, and run the following commands:
+
+```
+npm install
+grunt
+```
+
+The output will now be in the /dist folder. Source codes will be in /dist/src,
+and a zipped version will be in /dist.
+
+
+## Basic Architecture
+
+#### Important APIs
+
+To be filled out...
+
+#### Important Member Variables
+
+To be filled out...
+
+#### Constructor Arguments
+
+To be filled out...
+
+
+## Sample Usage
+
+To be filled out...
