a2a625ebb9b0f0f1002b3f34dd90f23313026d2d
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-07-03 15:38:03 -0700

    Fleshed out d.ts definitions

--- packages/stateholdr/Source/StateHoldr.d.ts
+++ packages/stateholdr/Source/StateHoldr.d.ts
@@ -5,6 +5,18 @@ declare module StateHoldr {
     }
 
     export interface IStateHoldr {
-
+        getItemsHolder(): ItemsHoldr.IItemsHoldr;
+        getPrefix(): string;
+        getCollectionKey(): string;
+        getCollectionKeyRaw(): string;
+        getCollection(): any;
+        getOtherCollection(otherCollectionKeyRaw: string): void;
+        getChanges(itemKey: string): any;
+        getChange(itemKey: string, valueKey: string): any;
+        setCollection(collectionKeyRawNew: string, value?: any): void;
+        saveCollection(): void;
+        addChange(itemKey: string, valueKey: string, value: any): void;
+        addCollectionChange(collectionKeyOtherRaw: string, itemKey: string, valueKey: string, value: any): void;
+        applyChanges(itemKey: string, output: any): void;
     }
 }
