7f65e91935f0107bb156fc71e2eb593b27137666
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-07-03 15:52:40 -0700

    Added basic collection usage tests

--- packages/stateholdr/Tests/tests.js
+++ packages/stateholdr/Tests/tests.js
@@ -2,8 +2,10 @@ var ItemsHolder = new ItemsHoldr.ItemsHoldr(
     {
 
     }),
+    StateHolder,
     prefix = "StateHoldrText",
-    StateHolder;
+    collectionKey = "FirstCollection",
+    collection;
 
 describe("constructor", function () {
     it("throws an error when not given an ItemsHoldr", function () {
@@ -25,5 +27,28 @@ describe("constructor", function () {
 
     it("stores the prefix", function () {
         chai.expect(StateHolder.getPrefix()).to.be.equal(prefix);
+    });
+});
+
+describe("basic collection usage", function () {
+    it("sets the current collection without error", function () {
+        StateHolder.setCollection(collectionKey);
+    });
+
+    it("gets the current collection without error", function () {
+        collection = StateHolder.getCollection();
+        chai.expect(collection).to.not.be.undefined;
+    });
+
+    it("gets the current collection by key", function () {
+        chai.expect(StateHolder.getOtherCollection(collectionKey)).to.be.equal(collection);
+    });
+
+    it("gets the current key", function () {
+        chai.expect(StateHolder.getCollectionKeyRaw()).to.be.equal(collectionKey);
+    });
+
+    it("gets the current key with the prefix", function () {
+        chai.expect(StateHolder.getCollectionKey()).to.be.equal(prefix + collectionKey);
     });
 });
