bc8a92addbf8c740412f4f905ee3559919c0e038
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-07-26 00:08:37 -0700

    Added initial blank .ts, .d.ts

---
+++ packages/devicelayr/Source/GamepadWrapperModule.d.ts
@@ -0,0 +1,9 @@
+declare module GamepadWrapperModule {
+    export interface IGamepadWrapperModuleSettings {
+
+    }
+
+    export interface IGamepadWrapperModule {
+
+    }
+}
---
+++ packages/devicelayr/Source/GamepadWrapperModule.ts
@@ -0,0 +1,16 @@
+// @ifdef INCLUDE_DEFINITIONS
+/// <reference path="GamepadWrapperModule.d.ts" />
+// @endif
+
+// @include ../Source/GamepadWrapperModule.d.ts
+
+module GamepadWrapperModule {
+    "use strict";
+
+    /**
+     * 
+     */
+    export class GamepadWrapperModule implements IGamepadWrapperModule {
+
+    }
+}
