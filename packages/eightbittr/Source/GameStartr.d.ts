d43624b55d2f0690d028e097b6186f69d16e890c
Josh Goldberg <josh@fullscreenmario.com>
2015-11-03 19:10:10 -0800

    Brought in settings.maps commands from Pokemon

--- packages/eightbittr/Source/GameStartr.d.ts
+++ packages/eightbittr/Source/GameStartr.d.ts
@@ -266,6 +266,8 @@ declare module GameStartr {
         "screenVariables": { [i: string]: Function };
         "onSpawn": (prething: MapsCreatr.IPreThing) => void;
         "onUnspawn": (prething: MapsCreatr.IPreThing) => void;
+        "stretchAdd"?: (thing: string | MapsCreatr.IPreThingSettings, index: number) => void;
+        "afterAdd"?: (thing: string | MapsCreatr.IPreThingSettings, index: number) => void;
         "macros": { [i: string]: MapsCreatr.IMapsCreatrMacro };
         "entrances": { [i: string]: MapsCreatr.IMapsCreatrEntrance };
         "patterns"?: any;
@@ -329,6 +331,40 @@ declare module GameStartr {
 
     export interface IWorldSeedrCustoms extends IGameStartrSettingsObject {
         possibilities: WorldSeedr.IPossibilityContainer;
+    }
+
+    export interface IThing extends EightBittr.IThing, LevelEditr.IThing, QuadsKeepr.IThing {
+        GameStarter: IGameStartr;
+        name: string;
+        groupType: string;
+        className: string;
+        alive?: boolean;
+        placed?: boolean;
+        changed?: boolean;
+        maxquads: number;
+        quadrants: QuadsKeepr.IQuadrant[];
+        imageData: ImageData;
+        attributes?: any;
+        spriteCycle?: any[];
+        spriteCycleSynched?: any[];
+        xvelOld?: number;
+        yvelOld?: number;
+        parallaxHoriz?: number;
+        parallaxVert?: number;
+        flipHoriz?: boolean;
+        flipVert?: boolean;
+        noshiftx?: boolean;
+        noshifty?: boolean;
+        nofall?: boolean;
+        nofallOld?: boolean;
+        nocollide?: boolean;
+        nocollideOld?: boolean;
+        movement?: Function;
+        movementOld?: Function;
+        onThingAdd?: Function;
+        onThingAdded?: Function;
+        onThingMake?: Function;
+        onDelete?: Function;
     }
 
     export interface IGameStartr extends EightBittr.IEightBittr {
@@ -438,40 +474,6 @@ declare module GameStartr {
         arrayDeleteThing(thing: IThing, array: any[], location?: number): void;
         takeScreenshot(name: string, format?: string): void;
         addPageStyles(styles: any): void;
-    }
-
-    export interface IThing extends EightBittr.IThing, LevelEditr.IThing, QuadsKeepr.IThing {
-        GameStarter: IGameStartr;
-        name: string;
-        groupType: string;
-        className: string;
-        alive?: boolean;
-        placed?: boolean;
-        changed?: boolean;
-        maxquads: number;
-        quadrants: QuadsKeepr.IQuadrant[];
-        imageData: ImageData;
-        attributes?: any;
-        spriteCycle?: any[];
-        spriteCycleSynched?: any[];
-        xvelOld?: number;
-        yvelOld?: number;
-        parallaxHoriz?: number;
-        parallaxVert?: number;
-        flipHoriz?: boolean;
-        flipVert?: boolean;
-        noshiftx?: boolean;
-        noshifty?: boolean;
-        nofall?: boolean;
-        nofallOld?: boolean;
-        nocollide?: boolean;
-        nocollideOld?: boolean;
-        movement?: Function;
-        movementOld?: Function;
-        onThingAdd?: Function;
-        onThingAdded?: Function;
-        onThingMake?: Function;
-        onDelete?: Function;
     }
 }
 
