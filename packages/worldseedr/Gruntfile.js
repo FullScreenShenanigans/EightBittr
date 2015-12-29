8e46b599b94c23d3c24d1d1ffbdef448a319abbd
Josh Goldberg <josh@fullscreenmario.com>
2015-12-29 14:56:38 -0500

    Updated TS to two-step builds (source & combined)

--- packages/worldseedr/Gruntfile.js
+++ packages/worldseedr/Gruntfile.js
@@ -4,36 +4,52 @@ module.exports = function (grunt) {
         "meta": {
             "paths": {
                 "source": "Source",
+                "build": "Build",
                 "dist": "Distribution"
             }
         },
         "tslint": {
-            "options": {
-                "configuration": grunt.file.readJSON("tslint.json")
-            },
-            "files": {
-                "src": ["<%= meta.paths.source %>/*.ts"]
-            }
-        },
-        "typescript": {
-            "base": {
-                "src": "<%= meta.paths.source %>/<%= pkg.name %>.ts"
-            }
-        },
-        "clean": ["<%= meta.paths.dist %>"],
-        "copy": {
             "default": {
+                "options": {
+                    "configuration": grunt.file.readJSON("tslint.json")
+                },
+                "files": {
+                    "src": ["<%= meta.paths.source %>/*.ts"]
+                }
+            }
+        },
+        "clean": {
+            "prebuild": [
+                "<%= meta.paths.dist %>",
+                "<%= meta.paths.build %>"
+            ],
+            "postbuild": [
+                "<%= meta.paths.dist %>/<%= pkg.name %>.js",
+                "<%= meta.paths.build %>"
+            ]
+        },
+        "copy": {
+            "prebuild": {
                 "files": [{
-                    "src": "<%= meta.paths.source %>/<%= pkg.name %>.js",
-                    "dest": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.js"
+                    "src": "<%= meta.paths.source %>/*.ts",
+                    "dest": "<%= meta.paths.build %>/",
+                    "expand": true,
+                    "flatten": true
                 }, {
                     "src": "<%= meta.paths.source %>/<%= pkg.name %>.ts",
-                    "dest": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.ts"
-                }, {
-                    "src": "<%= meta.paths.source %>/<%= pkg.name %>.d.ts",
-                    "dest": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.d.ts"
+                    "dest": "<%= meta.paths.build %>/",
+                    "expand": true,
+                    "flatten": true,
+                    "rename": function (dest, src) {
+                        return dest + "/<%= pkg.name %>-<%= pkg.version %>.ts";
+                    }
                 }, {
                     "src": "<%= meta.paths.source %>/References/*.ts",
+                    "dest": "<%= meta.paths.build %>",
+                    "expand": true,
+                    "flatten": true
+                }, {
+                    "src": "<%= meta.paths.source %>/References/*.?s",
                     "dest": "<%= meta.paths.dist %>/",
                     "expand": true,
                     "flatten": true
@@ -41,15 +57,29 @@ module.exports = function (grunt) {
                     "src": "README.md",
                     "dest": "<%= meta.paths.dist %>/"
                 }, {
-                    "src": "README.md",
-                    "dest": "<%= meta.paths.source %>/"
-                }, {
                     "src": "LICENSE.txt",
                     "dest": "<%= meta.paths.dist %>/"
-                }, {
-                    "src": "LICENSE.txt",
-                    "dest": "<%= meta.paths.source %>/"
                 }]
+            },
+            "distribution": {
+                "files": [{
+                    "src": "<%= meta.paths.source %>/<%= pkg.name %>.js",
+                    "dest": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.js"
+                }]
+            }
+        },
+        "preprocess": {
+            "default": {
+                "src": "<%= meta.paths.build %>/<%= pkg.name %>.ts",
+                "dest": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.ts"
+            }
+        },
+        "typescript": {
+            "source": {
+                "src": "<%= meta.paths.source %>/*.ts"
+            },
+            "distribution": {
+                "src": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.ts"
             }
         },
         "uglify": {
@@ -57,31 +87,27 @@ module.exports = function (grunt) {
                 "compress": true,
                 "sourceMap": true
             },
-            "dist": {
+            "default": {
                 "files": {
-                    "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.min.js": ["<%= meta.paths.source %>/<%= pkg.name %>.js"],
+                    "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.min.js": [
+                        "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.js"
+                    ]
                 }
             }
         },
-        "preprocess": {
-            "dist": {
-                "src": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.ts",
-                "dest": "<%= meta.paths.dist %>/<%= pkg.name %>-<%= pkg.version %>.ts"
-            }
-        },
         "mocha_phantomjs": {
-            "all": ["Tests/*.html"]
+            "default": ["Tests/*.html"]
         }
     });
     
+    grunt.loadNpmTasks("grunt-tslint");
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-preprocess");
+    grunt.loadNpmTasks("grunt-typescript");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-mocha-phantomjs");
-    grunt.loadNpmTasks("grunt-preprocess");
-    grunt.loadNpmTasks("grunt-tslint");
-    grunt.loadNpmTasks("grunt-typescript");
     grunt.registerTask("default", [
-        "tslint", "typescript", "clean", "copy", "uglify", "preprocess", "mocha_phantomjs"
+        "tslint", "clean", "copy:prebuild", "preprocess", "typescript:source", "copy:distribution", "typescript:distribution", "clean:postbuild", "uglify", "mocha_phantomjs"
     ]);
 };
