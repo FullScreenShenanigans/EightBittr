51e8c4c044138536d86a18fbc76061332666a7fe
Josh Goldberg <joshuakgoldberg@outlook.com>
2016-07-25 20:37:39 -0700

    Merge pull request #5 from FullScreenShenanigans/gulp-shenanigans
    Moved build process to gulp-shenanigans

---
+++ packages/stringfilr/test/utils/mocks.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../lib/StringFilr.d.ts" />
+
+const mocks = {
+    /**
+     * @param settings   Settings for the StringFilr.
+     * @returns An StringFilr instance.
+     */
+    mockStringFilr: (settings?: StringFilr.IStringFilrSettings): StringFilr.IStringFilr => {
+        return new StringFilr.StringFilr(settings);
+    }
+};
