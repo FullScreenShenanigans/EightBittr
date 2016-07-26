c8e2859e5c6649eee62c9ee488e5715610b5bd63
Josh Goldberg <josh@fullscreenmario.com>
2016-07-25 20:53:19 -0700

    Fixed generic types in test mocks

--- packages/stringfilr/test/utils/mocks.ts
+++ packages/stringfilr/test/utils/mocks.ts
@@ -5,7 +5,7 @@ const mocks = {
      * @param settings   Settings for the StringFilr.
      * @returns An StringFilr instance.
      */
-    mockStringFilr: (settings?: StringFilr.IStringFilrSettings): StringFilr.IStringFilr => {
+    mockStringFilr: (settings?: StringFilr.IStringFilrSettings<any>): StringFilr.IStringFilr<any> => {
         return new StringFilr.StringFilr(settings);
     }
 };
