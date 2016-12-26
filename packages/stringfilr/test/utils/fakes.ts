c0c519912c3a6ebf439f38f1ac4df55047d059f7
Josh Goldberg <joshuakgoldberg@outlook.com>
2016-12-26 03:30:06 -0500

    Merge pull request #6 from FullScreenShenanigans/6
    (v0.6.0) Bumped version to 0.6.0

--- packages/stringfilr/test/utils/fakes.ts
+++ packages/stringfilr/test/utils/fakes.ts
@@ -5,6 +5,6 @@ import { StringFilr } from "../../src/StringFilr";
  * @param settings   Settings for the StringFilr.
  * @returns An StringFilr instance.
  */
-export function mockStringFilr(settings: IStringFilrSettings<any>): IStringFilr<any> {
+export function stubStringFilr(settings: IStringFilrSettings<any>): IStringFilr<any> {
     return new StringFilr(settings);
 }
