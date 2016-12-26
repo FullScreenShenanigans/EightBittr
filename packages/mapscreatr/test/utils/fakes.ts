44c3c9cae8afd23d39f932605585b3efbc2dc9a6
Josh Goldberg <josh@fullscreenmario.com>
2016-12-26 07:29:20 -0500

    (v0.6.1) Node moduleResolution, ignored test setup files

---
+++ packages/mapscreatr/test/utils/fakes.ts
@@ -0,0 +1,11 @@
+import { IMapsCreatr, IMapsCreatrSettings } from "../../src/IMapsCreatr"
+import { MapsCreatr } from "../../src/MapsCreatr"
+
+export const fakes = {
+    /**
+     * 
+     */
+    stubMapsCreatr: (settings: IMapsCreatrSettings): IMapsCreatr => {
+        return new MapsCreatr(settings);
+    }
+};
