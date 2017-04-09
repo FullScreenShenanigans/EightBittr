8f1ed677490a2eae07e0a55ea80c520b9897add2
Josh Goldberg <josh@fullscreenmario.com>
2017-04-09 16:09:43 -0700

    (v0.6.4) Added enter, escape, and backspace keys

--- packages/userwrappr/src/UserWrappr.ts
+++ packages/userwrappr/src/UserWrappr.ts
@@ -19,7 +19,8 @@ export class UserWrappr implements IUserWrappr {
     public static allPossibleKeys: string[] = [
         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
         "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-        "up", "right", "down", "left", "space", "shift", "ctrl"
+        "up", "right", "down", "left", "space", "shift", "ctrl", "enter",
+        "escape", "backspace"
     ];
 
     /**
