ff9a3d9da00e641313920a843c4f727f8f29c4d8
Josh Goldberg <josh@fullscreenmario.com>
2017-04-24 13:26:48 -0700

    Added runGulpSetup(InAll) commands
    Fixes #5.

---
+++ packages/shenanigans-manager/src/commands/runGulpSetupIn.ts
@@ -0,0 +1,30 @@
+import { Command, ICommandArgs } from "../command";
+import { Shell } from "../shell";
+import { EnsureRepositoryExists } from "./ensureRepositoryExists";
+
+/**
+ * Arguments for a RunGulpIn command.
+ */
+export interface IRunGulpInArgs extends ICommandArgs {
+    /**
+     * Names of the repository.
+     */
+    repository: string;
+}
+
+/**
+ * Runs gulp-setup in a repository.
+ */
+export class RunGulpSetupIn extends Command<IRunGulpInArgs, void> {
+    /**
+     * Executes the command.
+     * 
+     * @returns A Promise for running the command.
+     */
+    public async execute(): Promise<any> {
+        await this.subroutine(EnsureRepositoryExists, this.args);
+
+        await new Shell(this.logger, this.args.directory, this.args.repository)
+            .execute("gulp setup");
+    }
+}
