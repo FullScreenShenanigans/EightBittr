621462167c1b95fea8a56b2220895d2878b639df
Josh Goldberg <josh@fullscreenmario.com>
2017-11-24 00:42:47 -0800

    Removed constructor
    Preparation for new UserWrappr architecture.

--- packages/eightbittr/test/utils/fakes.ts
+++ packages/eightbittr/test/utils/fakes.ts
@@ -1,10 +1,5 @@
 import { GameStartr } from "../../src/GameStartr";
-import { ISizeSettings } from "../../src/IGameStartr";
 
-export function stubGameStartr(settings?: ISizeSettings): GameStartr {
-    return new GameStartr({
-        width: 256,
-        height: 256,
-        ...settings
-    });
+export function stubGameStartr(): GameStartr {
+    return new GameStartr();
 }
