d2f51e992c42ce2cab931e687e48a60e6313c2c2
Josh Goldberg <josh@fullscreenmario.com>
2017-12-22 16:34:18 -0500

    (v0.7.1) Added missing src/index.ts

---
+++ packages/itemsholdr/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./createPlaceholderStorage";
+export * from "./IItemsHoldr";
+export * from "./IItemValue";
+export * from "./ItemsHoldr";
+export * from "./ITemValue";
+export * from "./proliferate";
