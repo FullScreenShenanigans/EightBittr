4fdf3ece4576cd9aa0619c44fde2bc21c05e1a77
Josh Goldberg <josh@fullscreenmario.com>
2018-01-14 18:12:13 -0800

    package-fixes

--- packages/eightbittr/src/creators/createObjectMaker.ts
+++ packages/eightbittr/src/creators/createObjectMaker.ts
@@ -4,6 +4,5 @@ import { GameStartr } from "../GameStartr";
 
 export const createObjectMaker = (gameStarter: GameStartr) =>
     new ObjectMakr({
-        doPropertiesFull: true,
         ...gameStarter.settings.components.objects,
     });
