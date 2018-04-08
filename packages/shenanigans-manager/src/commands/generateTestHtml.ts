6cce6664909051ecb5cc85de157a9a018287fc3d
Josh Goldberg <joshuakgoldberg@outlook.com>
2018-04-08 07:52:05 -0400

    Merge pull request #50 from FullScreenShenanigans/coverage
    Added basic istanbul report generation

--- packages/shenanigans-manager/src/commands/generateTestHtml.ts
+++ packages/shenanigans-manager/src/commands/generateTestHtml.ts
@@ -8,9 +8,19 @@ import { getDependencyNamesAndExternalsOfPackage, globAsync, parseFileJson } fro
 import { EnsureRepositoryExists } from "./ensureRepositoryExists";
 
 /**
+ * Arguments for a GenerateTestHtml command.
+ */
+export interface IGenerateTestHtmlArgs extends IRepositoryCommandArgs {
+    /**
+     * Directory to load source files from, if not src.
+     */
+    source?: string;
+}
+
+/**
  * Generates the HTML page for a repository's tests.
  */
-export const GenerateTestHtml = async (runtime: IRuntime, args: IRepositoryCommandArgs) => {
+export const GenerateTestHtml = async (runtime: IRuntime, args: IGenerateTestHtmlArgs) => {
     defaultPathArgs(args, "directory", "repository");
 
     await EnsureRepositoryExists(runtime, args);
@@ -19,10 +29,14 @@ export const GenerateTestHtml = async (runtime: IRuntime, args: IRepositoryComma
     const basePackageContents = await parseFileJson<IShenanigansPackage>(basePackageLocation);
 
     const testTemplate = (await fs.readFile(path.resolve(__dirname, "../../setup/test.html"))).toString();
-    const testPaths = (await globAsync(path.resolve(args.directory, args.repository, "src/**/*.test.ts*")))
+    let testPaths = (await globAsync(path.resolve(args.directory, args.repository, "src/**/*.test.ts*")))
         .map((testPath) => testPath.replace(/\.test\.(tsx|ts)/gi, ".test.js"))
         .map((testPath) => path.join("..", path.relative(path.join(args.directory, args.repository), testPath))
             .replace(/\\/g, "/"));
+
+    if (args.source !== undefined) {
+        testPaths = testPaths.map((testPath: string): string => testPath.replace("/src/", `/${args.source}/`));
+    }
 
     const { dependencyNames, externals } = await getDependencyNamesAndExternalsOfPackage(basePackageLocation);
 
@@ -35,7 +49,11 @@ export const GenerateTestHtml = async (runtime: IRuntime, args: IRepositoryComma
             testPaths,
         });
 
+    const testFileName = args.source === undefined
+        ? "test/index.html"
+        : `test/index.${args.source}.html`;
+
     await fs.writeFile(
-        path.join(args.directory, args.repository, "test/index.html"),
+        path.join(args.directory, args.repository, testFileName),
         newTestContents);
 };
