164a3181675153569dcc2ea0cb60052e443ccb9e
Josh Goldberg <josh@fullscreenmario.com>
2018-05-31 08:47:25 -0700

    Unused import in createTimeHandler

--- packages/eightbittr/src/creators/createTimeHandler.ts
+++ packages/eightbittr/src/creators/createTimeHandler.ts
@@ -1,7 +1,6 @@
 import { TimeHandlr } from "timehandlr";
 
 import { EightBittr } from "../EightBittr";
-import { IThing } from "../IEightBittr";
 
 export const createTimeHandler = (eightBitter: EightBittr) =>
     new TimeHandlr(eightBitter.settings.components.timeHandler);
