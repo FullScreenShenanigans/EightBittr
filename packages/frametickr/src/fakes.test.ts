789a5e59b58c7e4c2aaac54b4a988b5855be9382
Josh Goldberg <josh@fullscreenmario.com>
2018-08-15 22:46:35 -0700

    Build fix: type cast spies in test

--- packages/frametickr/src/fakes.test.ts
+++ packages/frametickr/src/fakes.test.ts
@@ -1,14 +1,14 @@
 import * as lolex from "lolex";
 
 import { GamesRunnr } from "./GamesRunnr";
-import { IGamesRunnrSettings } from "./IGamesRunnr";
+import { IGamesRunnrSettings, ITickCanceller, ITickScheduler } from "./IGamesRunnr";
 
 export const stubGamesRunnr = (settings: Partial<IGamesRunnrSettings> = {}) => {
     const clock = lolex.createClock();
 
     const gamesRunner = new GamesRunnr({
-        tickCanceller: clock.clearTimeout,
-        tickScheduler: clock.setTimeout,
+        tickCanceller: clock.clearTimeout as ITickCanceller,
+        tickScheduler: clock.setTimeout as ITickScheduler,
         ...settings,
     });
 
